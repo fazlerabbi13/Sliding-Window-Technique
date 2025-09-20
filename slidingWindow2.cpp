a46624ff477f5702f9bb8598f01ebf464d244d0f
fazlerabbi13 <mohammad0132794@gmail.com>
2025-09-20 19:56:26 +0600

    O(n) complexity.

--- slidingWindow2.cpp
+++ slidingWindow2.cpp
@@ -30,8 +30,8 @@ int maxSum(vector<int>& arr, int k){
 }
 
 int main(){
-//     vector<int> arr = {5, 2, -1, 0, 3};
-//     int k = 3;
-//     cout << maxSum(arr, k);
-//     return 0;
-// }
+    vector<int> arr = {5, 2, -1, 0, 3};
+    int k = 3;
+    cout << maxSum(arr, k);
+    return 0;
+}
--- tempCodeRunnerFile.cpp
+++
@@ -1,5 +0,0 @@
-vector<int> arr = {5, 2, -1, 0, 3};
-//     int k = 3;
-//     cout << maxSum(arr, k);
-//     return 0;
-// }
